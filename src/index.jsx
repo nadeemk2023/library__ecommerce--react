26a908ce8ca2a28a71f1fe528914ad93095c9982
nadeemk2023 <nadeemekhan@gmail.com>
2024-12-22 00:34:14 -0800

    Add FontAwesome library and icon for navigation

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBars);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
