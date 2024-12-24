b6bd4c77c8ef33cc74fcc9a9165e9d03d0e18fc6
nadeemk2023 <nadeemekhan@gmail.com>
2024-12-23 23:03:42 -0800

    Add menu backdrop and navigation links to Nav component

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBars, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBars,
+  faShoppingCart,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
 
-library.add(faBars, faShoppingCart);
+library.add(faBars, faShoppingCart, faTimes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
